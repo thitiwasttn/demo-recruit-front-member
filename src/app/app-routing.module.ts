4393911fb2138dc4d7c64a9135338499388148b6
Thitiwas <55071973+thitiwasttn@users.noreply.github.com>
2021-10-18 14:00:33 +0700

    up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,11 @@ import {RouterModule, Routes} from '@angular/router';
 const routes: Routes = [{
   path: 'home',
   loadChildren:  () => import('./home/home.module').then(m => m.HomeModule)
-}
+},
+  {
+    path: '',
+    loadChildren:  () => import('./home/home.module').then(m => m.HomeModule)
+  }
 ];
 
 @NgModule({
